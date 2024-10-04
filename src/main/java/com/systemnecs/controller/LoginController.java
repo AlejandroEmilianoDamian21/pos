a68f7f58127915ce4f40af3d9cfb48804568c4b5
Emiliano Alejandro <alejandroemiliano21@gmail.com>
2024-10-04 15:55:40 -0300

    Finalizacion del Login

--- src/main/java/com/systemnecs/controller/LoginController.java
+++ src/main/java/com/systemnecs/controller/LoginController.java
@@ -3,12 +3,16 @@ package com.systemnecs.controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import com.systemnecs.dao.UsuarioDAO;
+import com.systemnecs.model.Usuario;
+import com.systemnecs.util.ConexionBD;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.HBox;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
@@ -31,13 +35,32 @@ public class LoginController implements Initializable {
     @FXML
     private JFXButton btnIniciar;
 
+    private ConexionBD conexionBD;
+
+    private UsuarioDAO usuarioDAO;
+
     public void initialize(URL location, ResourceBundle resources) {
 
     }
 
     @FXML
-    void login(ActionEvent event) {
-        System.out.println("Hola");
+    void login(ActionEvent event) throws SQLException {
+        String username = cjUsername.getText().trim();
+        String password = cjPassword.getText().trim();
+
+        conexionBD = new ConexionBD();
+        conexionBD.conectar();
+
+        usuarioDAO = new UsuarioDAO(conexionBD);
+        Usuario usuario =  usuarioDAO.getUsuario(username,password);
+
+        if(usuario == null){
+            System.out.println("El usuario o contraseña incorrecta");
+            return;
+        }
+        //Mostrar la vida del DASHBOARD
+        System.out.println("Dashboard");
+
     }
 
 }
